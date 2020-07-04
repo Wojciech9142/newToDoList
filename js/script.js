5cf6eee2f9cf9bec1dbab71e8b5e0c7d6bcba7a4
Wojciech Wolak <wolak.wojciech.fe@gmail.com>
2020-07-04 14:43:10 +0200

    added showing task list

---
+++ js/script.js
@@ -0,0 +1,45 @@
+{
+    const tasksList = [
+        {
+            content: "wstać",
+            done: true,
+        },
+        {
+            content: "zjeść",
+            done: false,
+        }
+    ];
+
+    const reloadList = () => {
+        const listElement = document.querySelector(".js-tasksList");
+
+        for(const task of tasksList){
+            listElement.innerHTML += `
+                ${(task.done) ? "<li class=\" taskList__item taskList__item--done \" >" :  "<li class=\" taskList__item \" >" }
+                    <div class="taskList__toggleButton">
+                        ✔
+                    </div> 
+                        ${task.content}
+                    <div class="taskList__removeButton">
+                        🗑
+                    </div>
+                </li> 
+            `;
+        }
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+        reloadList();
+    }
+
+    
+
+    init();
+}
